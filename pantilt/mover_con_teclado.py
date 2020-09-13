d9fe7958fcf7d551a44c7df01693a25abd243e10
fbacinello <francobacinello22@gmail.com>
2020-09-13 18:28:41 +0200

    arreglado algunas cosita, ya funciona casi decente

--- pantilt/mover_con_teclado.py
+++ pantilt/mover_con_teclado.py
@@ -9,7 +9,7 @@ import keyboard
 pos_tilt = -90
 pos_pan = -90
 bandera = True
-speed = 0.05
+speed = 0
 last_touch = 0
 
 
@@ -37,43 +37,47 @@ def mover_tilt(i):
 
 def mover_horizontal(direccion):
     global pos_pan
-    if direccion == 'left' and pos_pan > -90:
-        pos_pan -= 1
-    if direccion == 'right' and pos_pan < 90:
-        pos_pan += 1
+    if direccion == 'right' and pos_pan > -90:
+        pos_pan -= 2
+    if direccion == 'left' and pos_pan < 90:
+        pos_pan += 2
     mover_pan(pos_pan)    
 
 
 def mover_vertical(direccion):
     global pos_tilt
     if direccion == 'up' and pos_tilt > -90:
-        pos_tilt -= 1        
+        pos_tilt -= 2        
     if direccion == 'down' and pos_tilt < 90:
-        pos_tilt += 1
+        pos_tilt += 2
     mover_tilt(pos_tilt)
 
 def volver_posicion():
     print('volverpos')
     global pos_tilt
     for i in range(pos_tilt, -90, -1):
+        time.sleep(0.01)
         mover_tilt(i)
     global pos_pan
     for i in range(pos_pan, -90, -1):
+        time.sleep(0.01)
         mover_pan(i)
 
+def log():
+    global pos_tilt
+    global pos_pan
+    print('h', pos_pan,'v', pos_tilt)
 
 def on_press_handler(event):
     timestamp = event.time
     global last_touch
     global speed
-    if not time.time() - last_touch > speed:
+    if not timestamp - last_touch > speed:
         pass
     last_touch = time.time()
-    print(timestamp)
+    
 
     key = event.name
-    print(key)
-
     if key in ('left', 'right'):
         mover_horizontal(key)
     if key in ('up', 'down'):
@@ -83,6 +87,10 @@ def on_press_handler(event):
         bandera = False
         print('esc')
         volver_posicion()
+    
+    log()
+
+
 
 
 keyboard.on_press(on_press_handler)
