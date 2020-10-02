f0154e2704dad86d382527cc6da6ac13a10ecccf
fbacinello <francobacinello22@gmail.com>
2020-10-02 12:45:02 +0200

    acomodando un poco el LilaBox

--- electronic/lila_box.py
+++ electronic/lila_box.py
@@ -1,51 +1,60 @@
-# CamJam EduKit 2 - Sensors
-# Worksheet 5 - Movement
-
-# Import Python header files
 import RPi.GPIO as GPIO
 import time
+
 
 # Set the GPIO naming convention
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Set a variable to hold the GPIO Pin identity
-pinpir = 17
-GPIO.setup(22, GPIO.OUT) # parlante
+pin_sensor = 17
+GPIO.setup(pin_sensor, GPIO.IN)
+
+pin_rele = 22
+GPIO.setup(pin_rele, GPIO.OUT)
 
 print("PIR Module Test (CTRL-C to exit)")
-
-# Set pin as input
-GPIO.setup(pinpir, GPIO.IN)
 
 # Variables to hold the current and last states
 currentstate = 0
 previousstate = 0
+ultimo_movimiento = 0
+duracion_fuente = 5
+
+
+def prender_rele():
+    GPIO.output(pin_rele, GPIO.HIGH)
+
+
+def apagar_rele():
+    GPIO.output(pin_rele, GPIO.LOW)
 
 try:
     print("Waiting for PIR to settle ...")
     # Loop until PIR output is 0
-    while GPIO.input(pinpir) == 1:
+    while GPIO.input(pin_sensor) == 1:
         currentstate = 0
 
     print("    Ready")
     # Loop until users quits with CTRL-C
     while True:
         # Read PIR state
-        currentstate = GPIO.input(pinpir)
+        currentstate = GPIO.input(pin_sensor)
 
         # If the PIR is triggered
         if currentstate == 1 and previousstate == 0:
             print("    Motion detected!")
-            GPIO.output(22, GPIO.HIGH)
-            time.sleep(3)
-            GPIO.output(22, GPIO.LOW)
-            # Record previous state
+            ultimo_movimiento = time.time()
+            prender_rele()
             previousstate = 1
+
         # If the PIR has returned to ready state
         elif currentstate == 0 and previousstate == 1:
             print("    Ready")
             previousstate = 0
+
+        if time.time() - ultimo_movimiento > duracion_fuente:
+            apagar_rele()
 
         # Wait for 10 milliseconds
         time.sleep(0.01)
