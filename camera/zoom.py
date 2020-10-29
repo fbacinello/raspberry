f5acea7f53144925ecac1928da0a7843e06944df
fbacinello <francobacinello22@gmail.com>
2020-10-29 08:58:24 +0100

    acomodando un poco

--- camera/zoom.py
+++ camera/zoom.py
@@ -1,5 +1,5 @@
-import time_lapse_captures as cam
-import time
+import camera as cam
+import sys
 import threading
 import pygame
 
@@ -9,71 +9,61 @@ W = 1.0
 H = 1.0
 
 pygame.init()
-fps=30
-fpsclock=pygame.time.Clock()
-sur_obj=pygame.display.set_mode((180,180))
+fps = 30
+fpsclock = pygame.time.Clock()
+sur_obj = pygame.display.set_mode((180, 180))
 pygame.display.set_caption("Keyboard_Input")
-White=(255,255,255)
-p1=10
-p2=10
-step=5
+White = (255, 255, 255)
+p1 = 10
+p2 = 10
+step = 5
+
 
 def preview():
-    cam.start_preview(0)    
+    cam.start_preview(0)
+
 
 def mover_pygame():
     global X
     global Y
     global W
     global H
-    
+
     global p1
     global p2
-    
+
     sur_obj.fill(White)
-    pygame.draw.rect(sur_obj, (255,0,0), (p1, p2, 70, 65))
+    pygame.draw.rect(sur_obj, (255, 0, 0), (p1, p2, 70, 65))
     for eve in pygame.event.get():
-        if eve.type==pygame.QUIT:
-            volver_posicion()            
+        if eve.type == pygame.QUIT:
+            volver_posicion()
             pygame.quit()
             sys.exit()
     key_input = pygame.key.get_pressed()
-    
+
     if key_input[pygame.K_LEFT]:
         p1 -= step
         X += 0.1
-        W = 0.5
-        H = 0.5
+
     if key_input[pygame.K_RIGHT]:
         p1 += step
         Y += 0.1
-        W = 0.5
-        H = 0.5
-        
+
     if key_input[pygame.K_UP]:
         p2 -= step
         H -= 0.1
     if key_input[pygame.K_DOWN]:
         p2 += step
         W -= 0.1
-        
+
     pygame.display.update()
     fpsclock.tick(fps)
     cam.zoomear(X, Y, W, H)
     print((X, Y, W, H))
+
 
 t = threading.Thread(target=preview)
 t.start()
 
 while True:
     mover_pygame()
-
-# for i in range(0, 11):
-#     val = i/10
-#     print(val)    
-#     cam.zoomear(val, val, W, H)
-#     time.sleep(0.5)
-
-
-
-# cam.zoomear(X, Y, W, H)
--- pantilt/keyboard_handler.py
+++
@@ -1,9 +0,0 @@
-import keyboard
-
-def on_press_handler(event):
-    print(event.name)
-    
-keyboard.on_press(on_press_handler)
-
-while True:
-    pass
