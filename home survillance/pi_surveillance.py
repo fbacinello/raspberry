b84fbb17d19f6b5da2296f0318de1394e33e0fe5
fbacinello <francobacinello22@gmail.com>
2020-11-24 09:09:22 +0100

    prueba sin pasar conf.json

--- home survillance/pi_surveillance.py
+++ home survillance/pi_surveillance.py
@@ -15,12 +15,16 @@ import cv2
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-c", "--conf", required=True, help="path to the JSON configuration file")
+ap.add_argument("-c", "--conf", required=False, help="path to the JSON configuration file")
 args = vars(ap.parse_args())
 # filter warnings, load the configuration and initialize the Dropbox
 # client
 warnings.filterwarnings("ignore")
-conf = json.load(open(args["conf"]))
+print(args["conf"])
+if args["conf"]:
+    conf = json.load(open(args["conf"]))
+else:
+    conf = json.load(open("conf.json"))
 client = None
 
 # check to see if the Dropbox should be used
