187f260d1495131b87d4a0d6636db232c8135850
fbacinello <francobacinello22@gmail.com>
2021-03-05 10:40:51 +0100

    epaper display pic files refactor

--- bike/display/test.py
+++ bike/display/test.py
@@ -3,7 +3,7 @@
 import sys
 import os
 
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), '../pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
